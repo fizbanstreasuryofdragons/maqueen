f747e0b56c37eda91e58741cc48fb23591657a8d
fizbanstreasuryofdragons <167873534+fizbanstreasuryofdragons@users.noreply.github.com>
2024-05-28 13:34:45 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
-basic.pause(1000)
-maqueen.motorStop(maqueen.Motors.All)
 basic.forever(function () {
-	
+    for (let index = 0; index < 4; index++) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+        basic.pause(1000)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+    basic.pause(2000)
 })
